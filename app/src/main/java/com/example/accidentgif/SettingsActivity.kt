5319b1b3a91fb00e8883b55f12259aeae2e89557
Clement Viel <vielclement@gmail.com>
2024-04-09 22:01:23 +0200

    clean code

--- app/src/main/java/com/example/accidentgif/SettingsActivity.kt
+++ app/src/main/java/com/example/accidentgif/SettingsActivity.kt
@@ -3,16 +3,10 @@ package com.example.accidentgif
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.preference.Preference
 import androidx.preference.PreferenceManager
-import com.example.accidentgif.GifMaker
-private const val TAG = "setting activity"
 
 class SettingsActivity : AppCompatActivity() {
-
     private var gif = GifMaker.getInstance()
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
